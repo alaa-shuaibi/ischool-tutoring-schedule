da0887410d752f01b5500368f719dfff5137b24c
Alaa Shuaibi <55153524+shuaibi27@users.noreply.github.com>
2021-12-06 00:04:26 -0600

    implemented basic UI

---
+++ index.js
@@ -0,0 +1,38 @@
+let topic = {
+    name: "", // e.g., IS 101 or Python
+    type: "", // Subject or Skill
+    sunday: "",
+    monday: "",
+    tuesday: "",
+    wednesday: "",
+    thursday: "",
+    friday: "",
+    saturday: "",
+    resources: [] // List of links to resources for given topic
+}
+
+let subjects = {
+    is101: {
+        name: "IS 107",
+        sunday: "",
+        monday: "1 PM",
+        tuesday: "1 PM",
+        wednesday: "1 PM",
+        thursday: "1 PM",
+        friday: "",
+        saturday: "",
+        resources: [{name: "Google", url: "https://www.google.com/"}]
+    }
+}
+
+let skills = {
+    python: {
+        name = "Python",
+        monday: "1 PM",
+        tuesday: "1 PM",
+        wednesday: "1 PM",
+        thursday: "1 PM",
+        friday: "1 PM",
+        resources: [{name: "Google", url: "https://www.google.com/"}]
+    }
+}
