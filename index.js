6f8d5cdbe8518123efc54aa69d8244211105b359
Alaa Shuaibi <55153524+shuaibi27@users.noreply.github.com>
2021-12-06 23:44:58 -0600

    worked on resources section + cleaned up code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let topic = {
     name: "", // e.g., IS 101 or Python
-    type: "", // Subject or Skill
-    sunday: "",
+    type: "", // subject or skill
+    sunday: "", // Ignored for now.
     monday: "",
     tuesday: "",
     wednesday: "",
     thursday: "",
-    friday: "",
-    saturday: "",
-    resources: [] // List of links to resources for given topic
+    friday: "", // Ignored for now.
+    saturday: "", // Ignored for now.
+    resources: [] // List of links to resources for given topic.
 };
 
-// Compare funtion for sorting topics:
+// Compare funtion for sorting topics by name:
 function compareTopics(topic1, topic2) {
     if (topic1["name"] < topic2["name"]) return -1;
     if (topic1["name"] > topic2["name"]) return 1;
@@ -19,72 +19,66 @@ function compareTopics(topic1, topic2) {
 }
 
 // Loads subjects & skills into schedule tables:
-function loadSchedules(subjects = [], skills = []) {
-    subjects.sort(compareTopics);
-    skills.sort(compareTopics);
-
-    let subject_filter = document.getElementById("subject-filter");
-    let skill_filter = document.getElementById("skill-filter");
+function loadSchedules(topics) {
+    topics.sort(compareTopics);
+    // let subject_filter = document.getElementById("subject-filter");
+    // let skill_filter = document.getElementById("skill-filter");
     let subject_schedule = document.getElementById("subject-schedule");
     let skill_schedule = document.getElementById("skill-schedule");
 
-    for (let s of subjects) {
-        let row = subject_schedule.insertRow();
+    for (let t of topics) {
+        let row;
+        if (t["type"] == "subject") {
+            row = subject_schedule.insertRow();
+        } else if (t["type"] == "skill") {
+            row = skill_schedule.insertRow();
+        } else { continue; }
+
         let name = row.insertCell(0);
-        let mon = row.insertCell(1);
-        let tue = row.insertCell(2);
-        let wed = row.insertCell(3);
-        let thu = row.insertCell(4);
-
-        name.innerHTML = s["name"];
-        mon.innerHTML = s["monday"];
-        tue.innerHTML = s["tuesday"];
-        wed.innerHTML = s["wednesday"];
-        thu.innerHTML = s["thursday"];
-    }
-
-    for (let s of skills) {
-        let row = skill_schedule.insertRow();
-        let name = row.insertCell(0);
-        let mon = row.insertCell(1);
-        let tue = row.insertCell(2);
-        let wed = row.insertCell(3);
-        let thu = row.insertCell(4);
-
-        name.innerHTML = s["name"];
-        mon.innerHTML = s["monday"];
-        tue.innerHTML = s["tuesday"];
-        wed.innerHTML = s["wednesday"];
-        thu.innerHTML = s["thursday"];
+        name.innerHTML = "<a href=\"#resources-container\">" + t["name"] + "</a>";
+        name.onclick = function() { displayResources(t["name"]); };
+        row.insertCell(1).innerHTML = t["monday"];
+        row.insertCell(2).innerHTML = t["tuesday"];
+        row.insertCell(3).innerHTML = t["wednesday"];
+        row.insertCell(4).innerHTML = t["thursday"];
     }
 }
 
 // Switches between Subject schedule and Skill schedule:
 function switchSchedule() {
     let toggle = document.getElementById("toggle");
-    let subject_filter = document.getElementById("subject-filter");
-    let skill_filter = document.getElementById("skill-filter");
+    // let subject_filter = document.getElementById("subject-filter");
+    // let skill_filter = document.getElementById("skill-filter");
     let subject_schedule = document.getElementById("subject-schedule");
     let skill_schedule = document.getElementById("skill-schedule");
 
     if (toggle.innerHTML == "Schedule by Skill") {
         toggle.innerHTML = "Schedule by Subject";
-        subject_filter.style.display = "none";
-        skill_filter.style.display = "";
+        // subject_filter.style.display = "none";
+        // skill_filter.style.display = "";
         subject_schedule.style.display = "none";
         skill_schedule.style.display = "";
     } else {
         toggle.innerHTML = "Schedule by Skill";
-        subject_filter.style.display = "";
-        skill_filter.style.display = "none";
+        // subject_filter.style.display = "";
+        // skill_filter.style.display = "none";
         subject_schedule.style.display = "";
         skill_schedule.style.display = "none";
     }
 }
 
+// Displays the Resources section:
+function displayResources(topicName) {
+    document.getElementById("resources-title").innerHTML = "Resources for " + topicName;
+    
+    // TODO: Load resources into list:
+
+    document.getElementById("resources-container").style.display = "";
+}
+
 // The following code is used to hardcode data into the website.
 
-let subjects = [
+let topics = [
     {
         name: "IS 101",
         type: "subject",
@@ -112,10 +106,7 @@ let subjects = [
         resources: [{
             name: "Google", url: "https://www.google.com/"
         }]
-    }
-];
-
-let skills = [
+    },
     {
         name: "Python",
         type: "skill",
@@ -146,4 +137,4 @@ let skills = [
     }
 ];
 
-loadSchedules(subjects, skills);
+loadSchedules(topics);
