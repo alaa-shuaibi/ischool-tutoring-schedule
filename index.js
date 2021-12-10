5fd14b8880c1e1d7772ae4f05c544199556bc2c8
Alaa Shuaibi <55153524+shuaibi27@users.noreply.github.com>
2021-12-09 21:36:51 -0600

    finished resources section + minor changes

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-let topic = {
-    name: "", // e.g., IS 101 or Python
-    type: "", // subject or skill
-    sunday: "", // Ignored for now.
-    monday: "",
-    tuesday: "",
-    wednesday: "",
-    thursday: "",
-    friday: "", // Ignored for now.
-    saturday: "", // Ignored for now.
-    resources: [] // List of links to resources for given topic.
-};
-
 // Compare funtion for sorting topics by name:
 function compareTopics(topic1, topic2) {
     if (topic1["name"] < topic2["name"]) return -1;
@@ -18,29 +5,29 @@ function compareTopics(topic1, topic2) {
     return 0;
 }
 
-// Loads subjects & skills into schedule tables:
-function loadSchedules(topics) {
-    topics.sort(compareTopics);
+// Loads the schedule onto the website:
+function loadSchedule(schedule) {
+    schedule.sort(compareTopics);
     // let subject_filter = document.getElementById("subject-filter");
     // let skill_filter = document.getElementById("skill-filter");
     let subject_schedule = document.getElementById("subject-schedule");
     let skill_schedule = document.getElementById("skill-schedule");
 
-    for (let t of topics) {
+    for (let s of schedule) {
         let row;
-        if (t["type"] == "subject") {
+        if (s["type"] == "subject") {
             row = subject_schedule.insertRow();
-        } else if (t["type"] == "skill") {
+        } else if (s["type"] == "skill") {
             row = skill_schedule.insertRow();
         } else { continue; }
 
         let name = row.insertCell(0);
-        name.innerHTML = "<a href=\"#resources-container\">" + t["name"] + "</a>";
-        name.onclick = function() { displayResources(t["name"]); };
-        row.insertCell(1).innerHTML = t["monday"];
-        row.insertCell(2).innerHTML = t["tuesday"];
-        row.insertCell(3).innerHTML = t["wednesday"];
-        row.insertCell(4).innerHTML = t["thursday"];
+        name.innerHTML = "<a href=\"#resources-container\">" + s["name"] + "</a>";
+        name.onclick = function() { displayResources(s["name"]); };
+        row.insertCell(1).innerHTML = s["monday"];
+        row.insertCell(2).innerHTML = s["tuesday"];
+        row.insertCell(3).innerHTML = s["wednesday"];
+        row.insertCell(4).innerHTML = s["thursday"];
     }
 }
 
@@ -67,74 +54,261 @@ function switchSchedule() {
     }
 }
 
-// Displays the Resources section:
+// Displays the Resources section for the given topic:
 function displayResources(topicName) {
+    let ul = document.getElementById("resources-list");
+    let links = resources[topicName];
+
     document.getElementById("resources-title").innerHTML = "Resources for " + topicName;
-    
-    // TODO: Load resources into list:
+    ul.innerHTML = "";
+
+    if (links.length == 0) {
+        let li = document.createElement("li");
+        li.innerHTML = "No resources available."
+    } else {
+        for (let l of links) {
+            let li = document.createElement("li");
+            if (l.name == "") { li.innerHTML = "<a href=\"" + l.url +"\" target=\"_blank\">" + l.url + "</a>"; }
+            else { li.innerHTML = "<a href=\"" + l.url +"\" target=\"_blank\">" + l.name + "</a>"; }
+            ul.appendChild(li);
+            ul.appendChild(document.createElement("br"));
+            ul.appendChild(document.createElement("br"));
+        }
+    }
 
     document.getElementById("resources-container").style.display = "";
 }
 
 // The following code is used to hardcode data into the website.
 
-let topics = [
+let schedule = [
     {
-        name: "IS 101",
-        type: "subject",
-        sunday: "",
-        monday: "1 PM",
-        tuesday: "1 PM",
-        wednesday: "1 PM",
-        thursday: "1 PM",
-        friday: "",
-        saturday: "",
-        resources: [{
-            name: "Google", url: "https://www.google.com/"
-        }]
-    }, 
-    {
-        name: "IS 107",
-        type: "subject",
-        sunday: "",
-        monday: "1 PM",
-        tuesday: "1 PM",
-        wednesday: "1 PM",
-        thursday: "1 PM",
-        friday: "",
-        saturday: "",
-        resources: [{
-            name: "Google", url: "https://www.google.com/"
-        }]
-    },
-    {
-        name: "Python",
-        type: "skill",
-        sunday: "",
-        monday: "1 PM",
-        tuesday: "1 PM",
-        wednesday: "1 PM",
-        thursday: "1 PM",
-        friday: "",
-        saturday: "",
-        resources: [{
-            name: "Google", url: "https://www.google.com/"
-        }]
-    },
-    {
-        name: "JavaScript",
-        type: "skill",
-        sunday: "",
-        monday: "1 PM",
-        tuesday: "1 PM",
-        wednesday: "1 PM",
-        thursday: "1 PM",
-        friday: "",
-        saturday: "",
-        resources: [{
-            name: "Google", url: "https://www.google.com/"
-        }]
+        "name": "IS 101",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 202",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 203",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 204",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 205",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 206",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 407",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "IS 430",
+        "type": "subject",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Python",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "SQL",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Java",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "JavaScript",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "R",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "C/C++",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Figma",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Data Visualization",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Databases",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
+    },{
+        "name": "Math",
+        "type": "skill",
+        "sunday": "",
+        "monday": "",
+        "tuesday": "",
+        "wednesday": "",
+        "thursday": "",
+        "friday": "",
+        "saturday": ""
     }
 ];
 
-loadSchedules(topics);
+let resources = {
+    "IS 101": [],
+    "IS 202": [],
+    "IS 203": [],
+    "IS 204": [],
+    "IS 205": [],
+    "IS 206": [],
+    "IS 407": [],
+    "IS 430": [],
+    "Python": [
+        {"name": "", "url": "https://www.python.org/"},
+        {"name": "", "url": "https://www.learnpython.org/"},
+        {"name": "", "url": "https://www.pythonforbeginners.com/"},
+        {"name": "", "url": "https://python.swaroopch.com"},
+        {"name": "", "url": "https://developers.google.com/edu/python"}    ],
+    "SQL": [
+        {"name": "", "url": "https://sqlbolt.com/"},
+        {"name": "", "url": "https://www.khanacademy.org/computing/computer-programming/sql/"},
+        {"name": "", "url": "https://sqlzoo.net/wiki/SQL_Tutorial"}    ],
+    "Java": [
+        {"name": "", "url": "https://www.codecademy.com/catalog/language/java"},
+        {"name": "", "url": "https://www.geeksforgeeks.org/java/"},
+        {"name": "", "url": "https://www.codingninjas.com/v2/courses/online-java-course"}    ],
+    "JavaScript": [
+        {"name": "", "url": "https://www.codecademy.com/catalog/language/javascript"},
+        {"name": "", "url": "https://eloquentjavascript.net/"},
+        {"name": "", "url": "https://developer.mozilla.org/en-US/docs/Learn"}
+    ],
+    "R": [
+        {"name": "", "url": "https://r4ds.had.co.nz/index.html"}
+    ],
+    "C/C++": [
+        {"name": "", "url": "https://www.cplusplus.com/"},
+        {"name": "", "url": "https://www.geeksforgeeks.org/c-plus-plus/"},
+        {"name": "", "url": "https://www.learncpp.com/"}
+    ],
+    "Figma": [
+        {"name": "", "url": "https://www.figma.com/resources/learn-design/"}
+    ],
+    "Data Visualization": [
+        {"name": "", "url": "https://www.tableau.com/"}
+    ],
+    "Databases": [],
+    "Math": []
+};
+
+loadSchedule(schedule);
