1056b54ff769040ff117d731f8b5dc89b73ac5a6
Alaa Shuaibi <55153524+shuaibi27@users.noreply.github.com>
2022-03-22 20:35:03 -0500

    set up flask app & connected website to google sheets file via sheets api

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, jsonify
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = Flask(__name__)
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+
+@app.route('/')
+def index():
+    return render_template('index.html'), 200
+
+@app.route('/getSchedule')
+def getSchedule():
+    schedule_sheet = client.open('iSchool Tutoring Schedule').get_worksheet(0)
+    schedule_data = schedule_sheet.get_all_records()
+    #print(schedule_data)
+    return jsonify(schedule_data), 200
+
+@app.route('/getResources')
+def getResources():
+    resources_sheet = client.open('iSchool Tutoring Schedule').get_worksheet(1)
+    resources_data = resources_sheet.get_all_records()
+    #print(resources_data)
+    
+    resources_data_by_topic = {}
+
+    for r in resources_data:
+        if r['subjects'] == '' and r['skills'] == '':
+            continue
+        elif r['subjects'] == '':
+            topic_list = r['subjects'].replace(' ', '').lower().split(',')
+        elif r['skills'] == '':
+            topic_list = r['skills'].replace(' ', '').lower().split(',')
+        else:
+            topics = r['subjects'] + ',' + r['skills']
+            topic_list = topics.replace(' ', "").lower().split(',')
+        
+        for t in topic_list:
+            if t not in resources_data_by_topic:
+                resources_data_by_topic[t] = []
+            resources_data_by_topic[t].append({
+                'name': r['name'],
+                'url': r['url']
+            })
+    
+    return jsonify(resources_data_by_topic), 200
+
+@app.route('/getTutors')
+def getTutors():
+    tutors_sheet = client.open('iSchool Tutoring Schedule').get_worksheet(2)
+    tutors_data = tutors_sheet.get_all_records()
+    #print(tutors_data)
+    return jsonify(tutors_data), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/index.js
@@ -0,0 +1,121 @@
+const request = new XMLHttpRequest();
+resources = {};
+
+function loadData() {
+    request.open("GET", "http://127.0.0.1:5000/getSchedule");
+    request.responseType = 'json';
+    request.onload = function() {
+        if (request.status >= 400) return;
+
+        // Get Schedule
+        let schedule = request.response;
+        displaySchedule(schedule);
+
+        // Get Resources
+        request.open("GET", "http://127.0.0.1:5000/getResources");
+        request.responseType = 'json';
+        request.onload = function() {
+            if (request.status >= 400) return;
+            let data = request.response;
+            console.log(data);
+            resources = data;
+
+            /*
+            // Get Tutors
+            request.open("GET", "http://127.0.0.1:5000/getTutors");
+            request.responseType = 'json';
+            request.onload = function() {
+                if (request.status >= 400) return;
+                let data = request.response;
+                console.log(data);
+            };
+            request.send();
+            */
+        };
+        request.send();
+    };
+    request.send();
+}
+
+// Compare funtion for sorting topics by name:
+function compareTopics(topic1, topic2) {
+    if (topic1["name"] < topic2["name"]) return -1;
+    if (topic1["name"] > topic2["name"]) return 1;
+    return 0;
+}
+
+// Displays the schedule onto the website:
+function displaySchedule(schedule) {
+    schedule.sort(compareTopics);
+    // let subject_filter = document.getElementById("subject-filter");
+    // let skill_filter = document.getElementById("skill-filter");
+    let subject_schedule = document.getElementById("subject-schedule");
+    let skill_schedule = document.getElementById("skill-schedule");
+
+    for (let s of schedule) {
+        let row;
+        if (s["type"] == "subject") {
+            row = subject_schedule.insertRow();
+        } else if (s["type"] == "skill") {
+            row = skill_schedule.insertRow();
+        } else { continue; }
+
+        let name = row.insertCell(0);
+        name.innerHTML = "<a href=\"#resources-container\">" + s["name"] + "</a>";
+        name.onclick = function() { displayResources(s["name"]); };
+        row.insertCell(1).innerHTML = s["monday"];
+        row.insertCell(2).innerHTML = s["tuesday"];
+        row.insertCell(3).innerHTML = s["wednesday"];
+        row.insertCell(4).innerHTML = s["thursday"];
+    }
+}
+
+// Switches between Subject schedule and Skill schedule:
+function switchSchedule() {
+    let toggle = document.getElementById("toggle");
+    // let subject_filter = document.getElementById("subject-filter");
+    // let skill_filter = document.getElementById("skill-filter");
+    let subject_schedule = document.getElementById("subject-schedule");
+    let skill_schedule = document.getElementById("skill-schedule");
+
+    if (toggle.innerHTML == "Schedule by Skill") {
+        toggle.innerHTML = "Schedule by Subject";
+        // subject_filter.style.display = "none";
+        // skill_filter.style.display = "";
+        subject_schedule.style.display = "none";
+        skill_schedule.style.display = "";
+    } else {
+        toggle.innerHTML = "Schedule by Skill";
+        // subject_filter.style.display = "";
+        // skill_filter.style.display = "none";
+        subject_schedule.style.display = "";
+        skill_schedule.style.display = "none";
+    }
+}
+
+// Displays the Resources section for the given topic:
+function displayResources(topicName) {
+    key = topicName.replace(/\s/g, "").toLowerCase();
+    let ul = document.getElementById("resources-list");
+
+    document.getElementById("resources-title").innerHTML = "Resources for " + topicName;
+    ul.innerHTML = "";
+
+    if (key in resources) { 
+        let links = resources[key];
+        for (let l of links) {
+            let li = document.createElement("li");
+            if (l.name == "") { li.innerHTML = "<a href=\"" + l.url +"\" target=\"_blank\">" + l.url + "</a>"; }
+            else { li.innerHTML = "<a href=\"" + l.url +"\" target=\"_blank\">" + l.name + "</a>"; }
+            ul.appendChild(li);
+            ul.appendChild(document.createElement("br"));
+            ul.appendChild(document.createElement("br"));
+        }
+    } else {
+        ul.innerHTML = "No resources available."
+    }
+
+    document.getElementById("resources-container").style.display = "";
+}
+
+loadData();
